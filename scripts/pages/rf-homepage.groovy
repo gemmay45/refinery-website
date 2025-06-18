a2a3eb693fa4bb11452e9e4a417c856217d3cda7
Gemma Yung <gemma.yung@gmail.com>
2025-06-18 09:53:20 +0800

    "User gemma wrote content /scripts/pages/rf-homepage.groovy"

--- scripts/pages/rf-homepage.groovy
+++ scripts/pages/rf-homepage.groovy
@@ -3,7 +3,7 @@ package scripts.api
 import scripts.libs.Cookies
 //import scripts.api.SecurityServices
 
-def context = SecurityServices.createContext(applicationContext, request)
+//def context = SecurityServices.createContext(applicationContext, request)
 
 
 //def ucookie = Cookies.getCookieValue("username", request)
